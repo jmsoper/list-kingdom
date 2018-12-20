b6883a4cef0fbc89cff0e136c9010e7276ee13dd
jmsoper <jmsoper@gmail.com>
2018-12-20 15:35:30 -0500

    Allow user to specify a command

--- task.c
+++ task.c
@@ -3,8 +3,11 @@
 
 int main(void){
   //just setting up vars
-  int i=0;
+  int taskcmp=0;
+  int commandcmp=0;
+  int exitcmp=0;
   char command[100];
+  char taskname[100];
   FILE *ofile;
 
   //check if the file exists - we append if existing, create it not.
@@ -15,14 +18,24 @@ int main(void){
   //start the program loop
   do {
     printf("Please enter a task name. Type 'exit' to quit.\n");
-    scanf("%s", command);
-    i = strcmp(command, "exit");
-    if(i!=0){
-      fprintf(ofile, "%s ", command);
+    scanf("%s %s", command, taskname);
+    exitcmp = strcmp(command, "exit");
+    if(exitcmp!=0){
+      commandcmp = strcmp(command, "read");
+      if (commandcmp==0){
+        //read out the file and print to screen.
+        printf("reading out your tasks!!");
+      }
+      commandcmp = strcmp(command, "add");
+      if (commandcmp==0){
+        //add the command
+        // -> we have an error in this whole thing about taking in the wrong number of args
+        fprintf(ofile, "%s ", taskname);
+      }
     } else {
       printf("Exiting program.\n");
     }
-  } while (i!=0);
+  } while (exitcmp!=0);
 
   fclose(ofile);
   return 0;
